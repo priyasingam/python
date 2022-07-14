3b139b2c90f5598349b635e7e3eb8b57f47e2faf
priyasingam <109233101+priyasingam@users.noreply.github.com>
2022-07-14 13:27:22 +0530

    Add files via upload

---
+++ patterns using while loop.py
@@ -0,0 +1,420 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("* * *")
+    elif x==2:
+        print("*   *")
+    elif x==3:
+        print("* * *")
+    else:
+        print("*   *")
+    x=x+1
+
+        
+
+
+# In[11]:
+
+
+x=1
+while x<=5:
+    if x==1:
+        print("***") 
+    elif x==2:
+        print("*   *")
+    elif x==3:
+        print("*****")
+    elif x==4:
+        print("*    *")
+    else:
+         print("****")
+    x=x+1 
+
+
+# In[12]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("* * *")
+    elif x==2:
+        print("*")
+    else:
+        print("* * *")
+    x=x+1
+        
+        
+
+
+# In[16]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("* * * ")
+    elif x==2:
+        print("*     *")
+    elif x==3:
+        print("*     *")
+    else:
+        print("*   *")
+    x=x+1
+
+
+# In[22]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*****")
+    elif x==2:
+        print("****")
+    else:
+        print("* ")
+    x=x+1
+        
+
+
+# In[21]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("* * *  ")
+    elif x==2:
+        print("* *   ")
+    else:
+        print("* * * ")
+    x=x+1
+        
+
+
+# In[32]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("****") 
+        
+    elif x==2:
+        print("*    ")
+    elif x==3:
+        print("*  ***")
+        
+    else:
+        print("**** *")
+    
+    x=x+1
+        
+
+
+# In[9]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*   *")
+    elif x==2:
+        print("* * *")
+    else:
+        print("*   *")
+    x=x+1
+        
+
+
+# In[16]:
+
+
+x=1
+while x<=4:
+    print("*")
+    x=x+1
+
+
+# In[19]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("    *")
+    elif x==2:
+        print("    *")
+    else:
+        print("* * *")
+    x=x+1
+        
+
+
+# In[24]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*   *")
+    elif x==2:
+        print("* *")
+    else:
+        print("*   *")
+    x=x+1
+        
+
+
+# In[25]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*")
+    elif x==2:
+        print("*")
+    else:
+        print("* * *")
+    x=x+1
+        
+
+
+# In[97]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*   *")
+    elif x==2:
+        print("* * *")
+        
+    else:
+        print("*   *")
+    x=x+1
+        
+
+
+# In[27]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*   *")
+    elif x==2:
+        print("* * *")
+    else:
+        print("*   *")
+    x=x+1
+        
+
+
+# In[39]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("* * *") 
+        
+    elif x==2:
+        print("*   *")
+        
+    else:
+        print("* * *")
+    x=x+1
+        
+
+
+# In[59]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("* * * ")
+    elif x==2: 
+        print("*     *")
+    elif x==3:
+        print("*  * *")
+    else:
+        print("*   ")
+    x=x+1
+
+
+# In[80]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("* *  *  ")
+        
+    elif x==2:
+        print("*     *")
+        
+        
+    elif x==3:
+        print("*   * *")
+        
+        
+    else:
+        print("* * *  *")
+    x=x+1
+
+    
+
+
+# In[69]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("* * *")
+    elif x==2:
+        print("*    *")
+    elif x==3:
+        print("*  *   ")
+    else:
+        print("*    *")
+    
+    x=x+1
+
+
+# In[96]:
+
+
+x=1
+while x<=5:
+    if x==1:
+        print("****") 
+    elif x==2:
+        print("*    ")
+    elif x==3:
+        print(" ***")
+    elif x==4:
+        print("    *")
+    else:
+         print("****")
+    x=x+1 
+    
+    
+
+
+# In[83]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("* * *")
+    elif x==2:
+        print("  * ")
+    else:
+        print("  * ")
+    x=x+1
+
+
+# In[3]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*   *")
+    elif x==2:
+        print("*   *")
+    else:
+        print("* * *")
+    x=x+1
+
+
+# In[4]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*    *")
+    elif x==2:
+        print(" *  *")
+    else:
+        print("  *")
+    x=x+1
+
+
+# In[5]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*    *     *")
+    elif x==2:
+        print(" *  *  *  *")
+    else:
+        print("  *      *")
+    x=x+1
+
+
+# In[6]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("*    *")
+    elif x==2:
+        print("   *  ")
+    else:
+        print("*    *")
+    x=x+1
+
+
+# In[12]:
+
+
+x=1
+while x<=4:
+    if x==1:
+        print("*    *")
+    elif x==2:
+        print("   *  ")
+    elif x==3:
+        print("   *  ")
+    else:
+        print("   *  ")
+    
+    x=x+1
+
+
+# In[13]:
+
+
+x=1
+while x<=3:
+    if x==1:
+        print("* * *")
+    elif x==2:
+        print("  *  ")
+    else:
+        print("* * *")
+    x=x+1
+
+
+# In[ ]:
+
+
+
+
